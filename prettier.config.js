b6635cb210f0e326d857e533bb61f3e18197b38d
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-08 15:33:49 -0600

    refactor(format): configure prettier with tailwind and import sorting plugins

--- prettier.config.js
+++ prettier.config.js
@@ -1,17 +1,5 @@
 module.exports = {
   semi: false,
-  plugins: [
-    "@ianvs/prettier-plugin-sort-imports",
-    "prettier-plugin-tailwindcss",
-  ],
-  importOrder: [
-    "^(react/(.*)$)|^(react$)",
-    "^(next/(.*)$)|^(next$)",
-    "<THIRD_PARTY_MODULES>",
-    "",
-    "^@/(.*)$",
-    "^[./]",
-  ],
-  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-  importOrderTypeScriptVersion: "4.9.5",
+  plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+  importOrder: ["^@/(.*)$", "^[./]"],
 }
