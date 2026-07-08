ed1cb74a157f27ada02b7311c4ef039d7dda99ab
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-08 15:54:28 -0600

    fix(types): resolve SVG maskType DOM property error in Next 15 build

--- src/components/ToggleDarkMode.tsx
+++ src/components/ToggleDarkMode.tsx
@@ -34,7 +34,7 @@ export default function ToggleDarkMode() {
       >
         <mask
           id="mask0"
-          maskType="alpha"
+          style={{ maskType: "alpha" }}
           maskUnits="userSpaceOnUse"
           x="0"
           y="0"
