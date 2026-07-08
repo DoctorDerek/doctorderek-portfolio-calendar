b8936ce19d7323fb7310f0b5e8474767dc7789ff
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-08 15:54:08 -0600

    build(config): migrate prettier configuration to TypeScript (prettier.config.ts)

---
+++ prettier.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "prettier";
+
+const config: Config = {
+  semi: false,
+  plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+  importOrder: ["^@/(.*)$", "^[./]"],
+};
+
+export default config;
