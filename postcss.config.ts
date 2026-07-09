016de14a816c33564312159a5804afcfad5e6a1b
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-09 16:06:54 -0600

    chore(config): convert postcss config to explicitly typed typescript

---
+++ postcss.config.ts
@@ -0,0 +1,8 @@
+// Type loosely as there isn't a strict Next.js PostCSS export
+const config: { plugins: Record<string, unknown> } = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+}
+
+export default config
