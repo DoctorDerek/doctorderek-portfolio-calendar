2811ed824067edc98b1360384d48a4440611d407
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-09 16:12:04 -0600

    chore(config): move PostCSS config into package.json to eliminate root file

--- postcss.config.ts
+++
@@ -1,8 +0,0 @@
-// Type loosely as there isn't a strict Next.js PostCSS export
-const config: { plugins: Record<string, unknown> } = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-}
-
-export default config
