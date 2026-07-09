0892bb1478ce8a20b300db1d4f4107c9cfdb008a
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-08 18:19:13 -0600

    refactor(config): remove custom Webpack loaders to enable native Turbopack support

--- next.config.ts
+++ next.config.ts
@@ -13,33 +13,6 @@ const nextConfig: NextConfig = {
 
   pageExtensions: ["js", "jsx", "ts", "tsx"],
 
-  webpack: (config, { dev, isServer, webpack }) => {
-    config.module.rules.push({
-      test: /\.(png|jpe?g|gif|mp4)$/i,
-      use: [
-        {
-          loader: "file-loader",
-          options: {
-            publicPath: "/_next",
-            name: "static/media/[name].[hash].[ext]",
-          },
-        },
-      ],
-    })
-
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    })
-
-    config.plugins.push(
-      new webpack.ProvidePlugin({
-        React: "react",
-      }),
-    )
-
-    return config
-  },
 }
 
 export default nextConfig
