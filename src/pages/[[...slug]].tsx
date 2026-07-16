053a1a075f2680977aacd26f3ec9d9116c22de3e
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-16 15:07:59 -0600

    refactor(routing): remove untyped catch-all page props

--- src/pages/[[...slug]].tsx
+++ src/pages/[[...slug]].tsx
@@ -1,10 +1,10 @@
 import dynamic from "next/dynamic"
 
 const NextIndexWrapper = dynamic(
-  () => import("../components/NextIndexWrapper"),
+  () => import("@/components/NextIndexWrapper"),
   { ssr: false },
 )
 
-export default function Page(props: Record<string, unknown>) {
-  return <NextIndexWrapper {...props} />
+export default function Page() {
+  return <NextIndexWrapper />
 }
