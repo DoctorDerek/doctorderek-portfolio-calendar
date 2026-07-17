70db59e1c66e41d4546607ff57b890217fc028da
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-17 13:13:25 -0600

    fix(metadata): use Calendar as the canonical application name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Calendar App - React / Redux - Material UI</title>
+        <title>Calendar</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
